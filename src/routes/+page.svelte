0867ced5e5a1e776541c9831fc51cdc9129de8db
Connor van Spronssen <info@dobefu.com>
2025-01-04 12:21:01 +0100

    feat: Add a basic counter element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
+<script>
+  let count = $state(0)
+</script>
+
 <svelte:head>
   <title>Home | Research - SvelteKit</title>
   <meta name="description" content="Description" />
 </svelte:head>
 
 <h1 class="text-3xl font-medium">Research - SvelteKit</h1>
+
+<p class="py-4">Count: {count}</p>
+
+<button class="rounded-md px-4 py-2 bg-gray-200" onclick={() => count--}>
+  Decrement
+</button>
+
+<button
+  class="rounded-md px-4 py-2 bg-red-200 disabled:bg-gray-100"
+  disabled={count === 0}
+  onclick={() => (count = 0)}
+>
+  Reset
+</button>
+
+<button class="rounded-md px-4 py-2 bg-gray-200" onclick={() => count++}>
+  Increment
+</button>
