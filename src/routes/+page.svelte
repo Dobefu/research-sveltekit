24130a520a30025d3efc96590ed1de6596742660
Connor van Spronssen <info@dobefu.com>
2025-01-04 11:38:17 +0100

    feat: Add page metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+	<title>Research - SvelteKit</title>
+	<meta name="description" content="Description" />
+</svelte:head>
+
 <h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
