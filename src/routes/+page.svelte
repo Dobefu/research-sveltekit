be4adcb78b18a65e3b75334c166d2ba778dcbced
Connor van Spronssen <info@dobefu.com>
2025-01-04 12:30:24 +0100

    feat: Add some styling to the counter element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,20 +9,30 @@
 
 <h1 class="text-3xl font-medium">Research - SvelteKit</h1>
 
-<p class="py-4">Count: {count}</p>
+<div class="border p-4 inline-flex flex-col my-8 shadow-lg rounded-2xl">
+  <p class="pb-4 mx-auto">Count: {count}</p>
 
-<button class="rounded-md px-4 py-2 bg-gray-200" onclick={() => count--}>
-  Decrement
-</button>
+  <div class="flex gap-4">
+    <button
+      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
+      onclick={() => count--}
+    >
+      Decrement
+    </button>
 
-<button
-  class="rounded-md px-4 py-2 bg-red-200 disabled:bg-gray-100"
-  disabled={count === 0}
-  onclick={() => (count = 0)}
->
-  Reset
-</button>
+    <button
+      class="transition-all rounded-md px-4 py-2 bg-red-200 disabled:bg-gray-100 disabled:text-gray-500 shadow disabled:shadow-none"
+      disabled={count === 0}
+      onclick={() => (count = 0)}
+    >
+      Reset
+    </button>
 
-<button class="rounded-md px-4 py-2 bg-gray-200" onclick={() => count++}>
-  Increment
-</button>
+    <button
+      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
+      onclick={() => count++}
+    >
+      Increment
+    </button>
+  </div>
+</div>
