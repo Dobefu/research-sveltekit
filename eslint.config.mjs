fd64a8a2ae6380f1eedc5e654a1be53e7ffddded
Connor van Spronssen <info@dobefu.com>
2025-01-04 11:23:30 +0100

    fix: Resolve all Prettier issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,3 @@ const eslintConfig = [
 ]
 
 export default eslintConfig
-
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,13 +3,11 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: [
-    "prettier-plugin-tailwindcss",
-  ],
-  trailingComma: "all",
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'all',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-};
+}
 
-export default config;
+export default config
