8203c608bbc447d3a60930e89ca8e2536c986d0f
Connor van Spronssen <info@dobefu.com>
2025-01-04 11:43:26 +0100

    feat: Add some basic layout styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,4 @@
 	<meta name="description" content="Description" />
 </svelte:head>
 
-<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-medium">Research - SvelteKit</h1>
