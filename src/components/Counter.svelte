ee687343f7012b92f0bf2952ff07382dfc4f9801
Connor van Spronssen <info@dobefu.com>
2025-01-04 12:38:03 +0100

    feat: Move the counter to a separate file

---
+++ src/components/Counter.svelte
@@ -0,0 +1,31 @@
+<script>
+  let count = $state(0)
+</script>
+
+<div class="border p-4 inline-flex flex-col my-8 shadow-lg rounded-2xl">
+  <p class="pb-4 mx-auto">Count: {count}</p>
+
+  <div class="flex gap-4">
+    <button
+      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
+      onclick={() => count--}
+    >
+      Decrement
+    </button>
+
+    <button
+      class="transition-all rounded-md px-4 py-2 bg-red-200 disabled:bg-gray-100 disabled:text-gray-500 shadow disabled:shadow-none"
+      disabled={count === 0}
+      onclick={() => (count = 0)}
+    >
+      Reset
+    </button>
+
+    <button
+      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
+      onclick={() => count++}
+    >
+      Increment
+    </button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  let count = $state(0)
+  import Counter from '../components/Counter.svelte'
 </script>
 
 <svelte:head>
@@ -9,30 +9,4 @@
 
 <h1 class="text-3xl font-medium">Research - SvelteKit</h1>
 
-<div class="border p-4 inline-flex flex-col my-8 shadow-lg rounded-2xl">
-  <p class="pb-4 mx-auto">Count: {count}</p>
-
-  <div class="flex gap-4">
-    <button
-      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
-      onclick={() => count--}
-    >
-      Decrement
-    </button>
-
-    <button
-      class="transition-all rounded-md px-4 py-2 bg-red-200 disabled:bg-gray-100 disabled:text-gray-500 shadow disabled:shadow-none"
-      disabled={count === 0}
-      onclick={() => (count = 0)}
-    >
-      Reset
-    </button>
-
-    <button
-      class="transition-colors rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 shadow"
-      onclick={() => count++}
-    >
-      Increment
-    </button>
-  </div>
-</div>
+<Counter />
