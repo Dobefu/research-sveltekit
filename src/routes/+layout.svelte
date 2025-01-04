cd6d4c5e1e26cf73d88ab2295d58ed5586b1d1bd
Connor van Spronssen <info@dobefu.com>
2025-01-04 11:58:18 +0100

    feat: Add a sticky footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
   import "../app.css";
   import Header from '../components/layout/Header.svelte'
+  import Footer from '../components/layout/Footer.svelte'
 </script>
 
 <Header />
 
-<main class="px-8" id="main-content">
+<main class="px-8 flex-1" id="main-content">
   <slot />
 </main>
+
+<Footer />
