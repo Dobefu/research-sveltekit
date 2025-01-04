3372bc7e0f4ced486324143a40aa92b26f5d2624
Connor van Spronssen <info@dobefu.com>
2025-01-04 11:02:29 +0100

    feat: Add Prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+};
+
+export default config;
